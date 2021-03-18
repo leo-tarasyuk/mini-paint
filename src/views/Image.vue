4490612e3ca0a0d38f848e6df99813cf31fd4521
leo-tarasyuk <leonid.tarasyuk52@gmail.com>
2021-03-18 11:04:42 +0300

    Refactored code

--- src/views/Image.vue
+++ src/views/Image.vue
@@ -1,62 +1,62 @@
 <template>
-  <header>
-    <h2>New image</h2>
-    <button @click="backHome()">←</button>
-  </header>
-  <main>
-    <div class="image-size">
-      <div class="figure">
-        <button class="pencil" @click="figure = 'pencil'">∼</button>
-        <button class="line" @click="figure = 'line'">/</button>
-        <button class="circle" @click="figure = 'circle'">◯</button>
-        <button class="rectangle" @click="figure = 'rectangle'">▭</button>
-      </div>
-      <canvas ref="canvas" width="650" height="500"></canvas>
-      <div class="options">
-        <div class="options-for-figure">
-          <button class="color" @click="isColorWindow = !isColorWindow">
-            Color
-          </button>
-          <button class="size" @click="isSizeWindow = !isSizeWindow">
-            Size
-          </button>
-        </div>
-        <div v-if="isSizeWindow" class="options-for-figure">
-          <button @click="size = 1">1</button>
-          <button @click="size = 5">5</button>
-          <button @click="size = 10">10</button>
-          <button @click="size = 20">20</button>
-        </div>
-        <div v-if="isColorWindow" class="options-for-figure">
-          <button
-            class="btn-red btn-color"
-            @click="color = 'rgba(255,0,0,1)'"
-          ></button>
-          <button
-            class="btn-green btn-color"
-            @click="color = 'rgba(0,255,0,1)'"
-          ></button>
-          <button
-            class="btn-blue btn-color"
-            @click="color = 'rgba(0,0,255,1)'"
-          ></button>
-          <button
-            class="btn-black btn-color"
-            @click="color = 'rgba(0,0,0,1)'"
-          ></button>
-          <button
-            class="btn-white btn-color"
-            @click="color = 'rgba(255,255,255,1)'"
-          ></button>
-        </div>
-        <div class="options-for-figure">
-          <button class="clear" @click="clearPicture()">Clear</button>
-          <button class="save" @click="savePicture()">Save</button>
-        </div>
-      </div>
-    </div>
-  </main>
   <div>
+    <header>
+      <h2>New image</h2>
+      <button @click="backHome()">←</button>
+    </header>
+    <main>
+      <div class="image-size">
+        <div class="figure">
+          <button class="pencil" @click="figure = 'pencil'">∼</button>
+          <button class="line" @click="figure = 'line'">/</button>
+          <button class="circle" @click="figure = 'circle'">◯</button>
+          <button class="rectangle" @click="figure = 'rectangle'">▭</button>
+        </div>
+        <canvas ref="canvas" width="650" height="500"></canvas>
+        <div class="options">
+          <div class="options-for-figure">
+            <button class="color" @click="isColorWindow = !isColorWindow">
+              Color
+            </button>
+            <button class="size" @click="isSizeWindow = !isSizeWindow">
+              Size
+            </button>
+          </div>
+          <div v-if="isSizeWindow" class="options-for-figure">
+            <button @click="size = 1">1</button>
+            <button @click="size = 5">5</button>
+            <button @click="size = 10">10</button>
+            <button @click="size = 20">20</button>
+          </div>
+          <div v-if="isColorWindow" class="options-for-figure">
+            <button
+              class="btn-red btn-color"
+              @click="color = 'rgba(255,0,0,1)'"
+            ></button>
+            <button
+              class="btn-green btn-color"
+              @click="color = 'rgba(0,255,0,1)'"
+            ></button>
+            <button
+              class="btn-blue btn-color"
+              @click="color = 'rgba(0,0,255,1)'"
+            ></button>
+            <button
+              class="btn-black btn-color"
+              @click="color = 'rgba(0,0,0,1)'"
+            ></button>
+            <button
+              class="btn-white btn-color"
+              @click="color = 'rgba(255,255,255,1)'"
+            ></button>
+          </div>
+          <div class="options-for-figure">
+            <button class="clear" @click="clearPicture()">Clear</button>
+            <button class="save" @click="savePicture()">Save</button>
+          </div>
+        </div>
+      </div>
+    </main>
     <Modal
       v-if="stateForShowModal"
       @stateChanged="stateForShowModal = $event"
